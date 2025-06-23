695796417573f33f2899f5b1c9c1ad64d11641ff
Marwane Assou <marwane.assou@gmail.com>
2025-06-24 00:18:15 +0200

    hotjar

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,6 +44,18 @@ export default function RootLayout({
             gtag('config', 'AW-16983946654');
           `}
         </Script>
+        <Script id="hotjar-init" strategy="afterInteractive">
+          {`
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:6443684,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `}
+        </Script>
       </head>
       <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
         <noscript>
