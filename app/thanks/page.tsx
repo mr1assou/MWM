6c53937ab4fd39e70705690cbc5c429a903bcf96
Marwane Assou <marwane.assou@gmail.com>
2025-08-23 18:10:04 +0100

    yarbi tyssr

--- app/thanks/page.tsx
+++ app/thanks/page.tsx
@@ -69,7 +69,7 @@ const ThanksPage = () => {
                 </h2>
                 <div>
                   <div className="mb-6 md:mb-10 w-full overflow-hidden rounded">
-                    <div className="relative aspect-[97/60] w-full  sm:aspect-[50/31] 2xl:aspect-[50/32]">
+                    <div className="relative aspect-[97/63] w-full  sm:aspect-[50/31] 2xl:aspect-[50/32]">
                       <Image
                         src="/images/hero/email_image.png"
                         alt="Thank you image"
